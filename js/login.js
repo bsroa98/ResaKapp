23174712425975111cc9399680da7ffc58db0996
bsroa98 <bsroa98@ucatolica.edu.co>
2025-04-17 12:34:12 -0500

    Commit arquitectura base

---
+++ js/login.js
@@ -0,0 +1,5 @@
+// login.js
+
+console.log('Resa-K login.js cargado correctamente.');
+
+// Aquí luego pondremos la integración con Google Sign-In
---
+++ js/main.js
@@ -0,0 +1,5 @@
+// main.js
+
+console.log('Resa-K main.js cargado correctamente.');
+
+// Aquí luego pondremos funciones como cambiar página, cargar categorías dinámicamente, etc.
